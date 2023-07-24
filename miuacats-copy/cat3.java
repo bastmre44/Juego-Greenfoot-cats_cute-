d32bf856195623f42afa70862f412e874199e1f6
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2023-07-23 22:02:35 -0600

    creación base

---
+++ miuacats-copy/cat3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class cat3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class cat3 extends cat2
+{
+    /**
+     * Act - do whatever the cat3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
