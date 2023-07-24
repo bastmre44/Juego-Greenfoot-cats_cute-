f41c340ac94acd476f2f555b090a15af18891777
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2023-07-23 22:36:47 -0600

    imagenes para el juego

--- miuacats-copy/levels.java
+++ miuacats-copy/levels.java
@@ -17,5 +17,12 @@ public class levels extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
